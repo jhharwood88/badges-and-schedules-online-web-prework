9a599fbb76dffee34a0826f3f5453a7b19c63225
Jonathan Harwood <jhharwood88@gmail.com>
2018-11-26 02:06:01 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
 def badge_maker(attendees)
-  
+  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   attendees.each do |name|
     puts "Hello, my name is #{name}."
   end
