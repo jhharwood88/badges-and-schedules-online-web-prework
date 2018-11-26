2fdd3060b041ee72fb275171f474ca59fedba642
Jonathan Harwood <jhharwood88@gmail.com>
2018-11-26 03:02:26 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,5 +7,5 @@ def batch_badge_creator(attendees)
 end 
 
 def assign_rooms(attendees)
-   attendees.map! {|x| "Hello, #{x}! "} 
+   attendees.map! {|x| "Hello, #{x}! You'll be assigned to room #{y}"} 
 end
